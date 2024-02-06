94efd6eda0f8ac7cf3f2a860e27c402416407418
amadou-design <64911911+amadou-design@users.noreply.github.com>
2024-02-06 15:59:43 -0500

    Implementing Loadmodel

--- Source/Engine/Rendering/src/Resources/Model.cpp
+++ Source/Engine/Rendering/src/Resources/Model.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "SurvivantRendering/Resources/Model.h"
 
 
 bool Model::LoadModel(const std::string& p_filename)
 {
-    std::ifstream file(p_filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file: " << p_filename << std::endl;
-        return false;
-    }
+	std::ifstream file(p_filename);
+	if (!file.is_open()) 
+	{
+		std::cerr << "Error: Unable to open file: " << p_filename << std::endl;
+		return false;
+	}
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string type;
-        iss >> type;
+	std::string line;
+	while (std::getline(file, line)) 
+	{
+		std::istringstream iss(line);
+		std::string type;
+		iss >> type;
 
-        if (type == "v") {
-            float x, y, z;
-            iss >> x >> y >> z;
-            vertices.push_back(x);
-            vertices.push_back(y);
-            vertices.push_back(z);
-        }
-        else if (type == "vn") {
-            float nx, ny, nz;
-            iss >> nx >> ny >> nz;
-            normals.push_back(nx);
-            normals.push_back(ny);
-            normals.push_back(nz);
-        }
-        else if (type == "vt") {
-            float u, v;
-            iss >> u >> v;
-            UVs.push_back(u);
-            UVs.push_back(v);
-        }
-    }
+		if (type == "v")
+		{
+			float x, y, z;
+			iss >> x >> y >> z;
+			m_vertices.push_back(x);
+			m_vertices.push_back(y);
+			m_vertices.push_back(z);
+		}
+		else if (type == "vn")
+		{
+			float nx, ny, nz;
+			iss >> nx >> ny >> nz;
+			m_normals.push_back(nx);
+			m_normals.push_back(ny);
+			m_normals.push_back(nz);
+		}
+		else if (type == "vt")
+		{
+			float u, v;
+			iss >> u >> v;
+			m_UVs.push_back(u);
+			m_UVs.push_back(v);
+		}
+	}
 
-    file.close();
-    std::cout << "Model loaded successfully from file: " << filename << std::endl;
-    return true;
+	file.close();
+	std::cout <<"Model loaded successfully from file: "<< p_filename << std::endl;
+	return true;
 }
 
 void Model::RenderModel()
