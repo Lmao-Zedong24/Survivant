17fa38bc9666f3f45df825dcc593837358a438aa
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-02-09 07:23:12 -0500

    Update preview

--- Source/Test/src/main.cpp
+++ Source/Test/src/main.cpp
@@ -1,13 +1,16 @@
+#include "SurvivantCore/Debug/Assertion.h"
+#include "SurvivantCore/Utility/FileSystem.h"
+
+#include "SurvivantRendering/Core/Camera.h"
+#include "SurvivantRendering/Core/Color.h"
 #include "SurvivantRendering/Core/Vertex.h"
 #include "SurvivantRendering/Core/VertexAttributes.h"
 #include "SurvivantRendering/Core/Buffers/IndexBuffer.h"
 #include "SurvivantRendering/Core/Buffers/VertexBuffer.h"
+#include "SurvivantRendering/Geometry/BoundingBox.h"
 #include "SurvivantRendering/Resources/Shader.h"
 
 // TODO: Implement relevant parts in rendering lib to get rid of glad/glfw dependencies
-#include "SurvivantCore/Debug/Assertion.h"
-#include "SurvivantCore/Utility/FileSystem.h"
-
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
@@ -15,6 +18,7 @@ using namespace LibMath;
 using namespace SvCore::Utility;
 using namespace SvRendering::Core;
 using namespace SvRendering::Core::Buffers;
+using namespace SvRendering::Geometry;
 using namespace SvRendering::Resources;
 
 constexpr const char* UNLIT_SHADER_PATH = "assets/shaders/Unlit.glsl";
@@ -89,8 +93,50 @@ GLuint GetDefaultTexture()
     return textureId;
 }
 
+void DrawCube(const VertexAttributes& p_vao)
+{
+    p_vao.Bind();
+
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
+}
+
+BoundingBox GetCubeBoundingBox(const Matrix4& transform)
+{
+    BoundingBox boundingBox
+    {
+        { -.5f, -.5f, -.5f },
+        { .5f, .5f, .5f }
+    };
+
+    Vector3 corners[8]
+    {
+        boundingBox.m_min,
+        { boundingBox.m_min.m_x, boundingBox.m_max.m_y, boundingBox.m_min.m_z },
+        { boundingBox.m_min.m_x, boundingBox.m_min.m_y, boundingBox.m_max.m_z },
+        { boundingBox.m_min.m_x, boundingBox.m_max.m_y, boundingBox.m_max.m_z },
+        { boundingBox.m_max.m_x, boundingBox.m_min.m_y, boundingBox.m_max.m_z },
+        { boundingBox.m_max.m_x, boundingBox.m_max.m_y, boundingBox.m_min.m_z },
+        { boundingBox.m_max.m_x, boundingBox.m_min.m_y, boundingBox.m_min.m_z },
+        boundingBox.m_max
+    };
+
+    boundingBox.m_min = Vector3(std::numeric_limits<float>::max());
+    boundingBox.m_max = Vector3(std::numeric_limits<float>::lowest());
+
+    for (Vector3& corner : corners)
+    {
+        corner            = (transform * Vector4(corner, 1.f)).xyz();
+        boundingBox.m_min = min(boundingBox.m_min, corner);
+        boundingBox.m_max = max(boundingBox.m_max, corner);
+    }
+
+    return boundingBox;
+}
+
 int main()
 {
+    SvCore::Debug::Logger::GetInstance().SetFile("debug.log");
+
     ASSERT(SetWorkingDirectory(GetApplicationDirectory()), "Failed to update working directory");
     SV_LOG("Current working directory: \"%s\"", GetWorkingDirectory().c_str());
 
@@ -104,9 +150,7 @@ int main()
 
     ASSERT(gladLoadGL(glfwGetProcAddress), "Failed to initialize glad");
 
-    Shader shader;
-    ASSERT(shader.Load(UNLIT_SHADER_PATH), "Failed to load shader at path \"%s\"", UNLIT_SHADER_PATH);
-    ASSERT(shader.Init(), "Failed to initialize shader at path \"%s\"", UNLIT_SHADER_PATH);
+    glEnable(GL_DEPTH_TEST);
 
     const auto [vertices, indices] = MakeCube();
 
@@ -114,37 +158,67 @@ int main()
     const IndexBuffer      ebo(indices, 36); // 6 indices * 6 faces
     const VertexAttributes vao(vbo, ebo);
 
-    shader.Use();
-
-    vao.Bind();
-    vbo.Bind();
-    ebo.Bind();
-
     const GLuint textureId = GetDefaultTexture();
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, textureId);
 
-    const Matrix4 projMat     = perspectiveProjection(90_deg, 4.f / 3.f, .01f, 14.f);
-    const Matrix4 viewMat     = lookAt({ 0.f, 1.8f, 1.f }, Vector3::zero(), Vector3::up());
-    const Matrix4 viewProjMat = projMat * viewMat;
-    shader.SetUniformInt("u_diffuse", 0);
+    Shader unlitShader;
+    ASSERT(unlitShader.Load(UNLIT_SHADER_PATH), "Failed to load shader at path \"%s\"", UNLIT_SHADER_PATH);
+    ASSERT(unlitShader.Init(), "Failed to initialize shader at path \"%s\"", UNLIT_SHADER_PATH);
+
+    unlitShader.Use();
+    unlitShader.SetUniformInt("u_diffuse", 0);
+
+    const Matrix4 projMat = perspectiveProjection(90_deg, 4.f / 3.f, .01f, 14.f);
+
+    Vector3       camPos(0.f, 1.8f, 2.f);
+    const Vector3 testPos      = camPos + Vector3::front();
+    const Matrix4 testModelMat = translation(testPos) * scaling(.1f, .5f, 1.5f);
+
+    Camera cam(projMat);
 
     Degree angle;
 
-    glClearColor(0, 0, 0, 1);
+    cam.SetClearColor(Color::black);
 
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-        angle += .01_deg; // TODO: use delta time
+        angle += .011_deg; // TODO: use delta time
 
-        const Matrix4 modelMat = translation(0.f, .5f, 0.f) * rotation(angle, Vector3::up());
-        shader.SetUniformMat4("u_mvp", viewProjMat * modelMat);
+        const Matrix4 modelRot  = rotation(angle, Vector3::up());
+        const Matrix4 modelMat1 = translation(-1.f, .5f, 0.f);
+        const Matrix4 modelMat2 = translation(1.f, .5f, 0.f) * modelRot;
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
+        camPos.rotate(.1_deg, Vector3::up());
+        cam.SetView(lookAt(camPos, Vector3::zero(), Vector3::up()));
+        cam.Clear();
+
+        const Frustum camFrustum = cam.GetFrustum();
+
+        if (camFrustum.Intersects(testPos))
+            SV_LOG("Point %s is in cam frustum", testPos.string().c_str());
+
+        const Matrix4 viewProjection = cam.GetViewProjection();
+
+        unlitShader.Use();
+        unlitShader.SetUniformMat4("u_mvp", viewProjection * modelMat1);
+        unlitShader.SetUniformVec4("u_tint", Color::white);
+        DrawCube(vao);
+
+        unlitShader.SetUniformMat4("u_mvp", viewProjection * modelMat2);
+        unlitShader.SetUniformVec4("u_tint", Color::red);
+        DrawCube(vao);
+
+        if (camFrustum.Intersects(GetCubeBoundingBox(testModelMat)))
+        {
+            SV_LOG("Cullable model is visible");
+            unlitShader.SetUniformMat4("u_mvp", viewProjection * testModelMat);
+            unlitShader.SetUniformVec4("u_tint", Color::yellow);
+            DrawCube(vao);
+        }
 
         glfwSwapBuffers(window);
     }
