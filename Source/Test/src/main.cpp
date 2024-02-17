6ce00e7be1df6381d312f659677c00d7bdf9af5d
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-02-16 23:48:03 -0500

    Add camera move and rotate inputs

--- Source/Test/src/main.cpp
+++ Source/Test/src/main.cpp
@@ -15,6 +15,8 @@
 #include <SurvivantRendering/Resources/Shader.h>
 
 // TODO: Implement relevant parts in corresponding libs to get rid of glad/glfw dependencies
+#include <Transform.h>
+
 #include <glad/gl.h>
 
 #include "SurvivantTest/Window.h"
@@ -26,7 +28,9 @@ using namespace SvRendering::Core::Buffers;
 using namespace SvRendering::Geometry;
 using namespace SvRendering::Resources;
 
-constexpr const char* UNLIT_SHADER_PATH = "assets/shaders/Unlit.glsl";
+constexpr const char* UNLIT_SHADER_PATH  = "assets/shaders/Unlit.glsl";
+constexpr float       CAM_MOVE_SPEED     = 3.f;
+constexpr Radian      CAM_ROTATION_SPEED = 90_deg;
 
 std::pair<const Vertex*, const uint32_t*> MakeCube()
 {
@@ -188,9 +192,11 @@ int main()
 
     const Matrix4 projMat = perspectiveProjection(90_deg, 4.f / 3.f, .01f, 14.f);
 
-    Vector3       camPos(0.f, 1.8f, 2.f);
+    Vector3   camPos(0.f, 1.8f, 2.f);
+    Transform camTransform(camPos, Quaternion::identity(), Vector3::one());
+
     const Vector3 testPos      = camPos + Vector3::front();
-    const Matrix4 testModelMat = translation(testPos) * scaling(.1f, .5f, 1.5f);
+    const Matrix4 testModelMat = translation(testPos) * scaling(1.5f, .5f, .1f);
 
     Camera cam(projMat);
 
@@ -212,6 +218,7 @@ int main()
     {
         std::cout << "Add = " << i + j << std::endl;
     };
+
     std::shared_ptr<AddEvent> ligEvent = std::make_shared<AddEvent>();
     ligEvent->AddListener(printAdd);
     em.AddEvent<AddEvent>(ligEvent);
@@ -219,11 +226,104 @@ int main()
     InputManager::KeyboardKeyType a(EKey::A, EKeyState::RELEASED, EInputModifier::ALT);
     InputManager::KeyboardKeyType b(EKey::B, EKeyState::PRESSED, EInputModifier());
     InputManager::MouseKeyType    mouse(EMouseButton::MOUSE_1, EMouseButtonState::PRESSED, EInputModifier());
+
     im.AddInputEventBinding<AddEvent>(a, &AddInputTranslate);
     im.AddInputEventBinding<AddEvent>(b, &AddInputTranslate);
     //mouse, &AddMouseTranslate
     im.AddInputEventBinding<AddEvent>(mouse, &AddMouseTranslate);
     //im.CallInput(b, 'b');
+
+    Vector2 moveInput, rotateInput;
+
+    im.AddInputBinding({ EKey::W, EKeyState::PRESSED, EInputModifier() }, [&moveInput](const char)
+    {
+        ++moveInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::W, EKeyState::RELEASED, EInputModifier() }, [&moveInput](const char)
+    {
+        --moveInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::S, EKeyState::PRESSED, EInputModifier() }, [&moveInput](const char)
+    {
+        --moveInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::S, EKeyState::RELEASED, EInputModifier() }, [&moveInput](const char)
+    {
+        ++moveInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::A, EKeyState::PRESSED, EInputModifier() }, [&moveInput](const char)
+    {
+        --moveInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::A, EKeyState::RELEASED, EInputModifier() }, [&moveInput](const char)
+    {
+        ++moveInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::D, EKeyState::PRESSED, EInputModifier() }, [&moveInput](const char)
+    {
+        ++moveInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::D, EKeyState::RELEASED, EInputModifier() }, [&moveInput](const char)
+    {
+        --moveInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::UP, EKeyState::PRESSED, EInputModifier() }, [&rotateInput](const char)
+    {
+        ++rotateInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::UP, EKeyState::RELEASED, EInputModifier() }, [&rotateInput](const char)
+    {
+        --rotateInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::DOWN, EKeyState::PRESSED, EInputModifier() }, [&rotateInput](const char)
+    {
+        --rotateInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::DOWN, EKeyState::RELEASED, EInputModifier() }, [&rotateInput](const char)
+    {
+        ++rotateInput.m_y;
+    });
+
+    im.AddInputBinding({ EKey::LEFT, EKeyState::PRESSED, EInputModifier() }, [&rotateInput](const char)
+    {
+        --rotateInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::LEFT, EKeyState::RELEASED, EInputModifier() }, [&rotateInput](const char)
+    {
+        ++rotateInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::RIGHT, EKeyState::PRESSED, EInputModifier() }, [&rotateInput](const char)
+    {
+        ++rotateInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::RIGHT, EKeyState::RELEASED, EInputModifier() }, [&rotateInput](const char)
+    {
+        --rotateInput.m_x;
+    });
+
+    im.AddInputBinding({ EKey::R, EKeyState::RELEASED, {} }, [&camTransform, &camPos](const char)
+    {
+        camTransform.setAll(camPos, Quaternion::identity(), Vector3::one());
+    });
+
+    im.AddInputBinding({ EKey::ESCAPE, EKeyState::RELEASED, {} }, [&window](const char)
+    {
+        glfwSetWindowShouldClose(window, true);
+    });
 
     while (!glfwWindowShouldClose(window))
     {
@@ -236,15 +336,32 @@ int main()
         const Matrix4 modelMat1 = translation(-1.f, .5f, 0.f);
         const Matrix4 modelMat2 = translation(1.f, .5f, 0.f) * modelRot;
 
-        camPos.rotate(20_deg * timer.getDeltaTime(), Vector3::up());
-        cam.SetView(lookAt(camPos, Vector3::zero(), Vector3::up()));
+        Vector3    newPos = camTransform.getPosition();
+        Quaternion newRot = camTransform.getRotation();
+
+        if (moveInput.magnitudeSquared() > 0.f)
+        {
+            const Vector3 moveDir = moveInput.m_x * camTransform.worldRight() + moveInput.m_y * camTransform.worldBack();
+            newPos += moveDir.normalized() * (CAM_MOVE_SPEED * timer.getDeltaTime());
+        }
+
+        if (rotateInput.magnitudeSquared() > 0.f)
+        {
+            TVector3<Radian> angles = newRot.toEuler(ERotationOrder::YXZ);
+
+            angles.m_y -= CAM_ROTATION_SPEED * rotateInput.m_x * timer.getDeltaTime();
+            angles.m_x += CAM_ROTATION_SPEED * rotateInput.m_y * timer.getDeltaTime();
+            angles.m_x = Degree(clamp(angles.m_x.degree(true), -85.f, 85.f));
+
+            newRot = Quaternion::fromEuler(angles, ERotationOrder::YXZ);
+        }
+
+        camTransform.setAll(newPos, newRot, Vector3::one());
+
+        cam.SetView(camTransform.getWorldMatrix().inverse());
         cam.Clear();
 
-        const Frustum camFrustum = cam.GetFrustum();
-
-        if (camFrustum.Intersects(testPos))
-            SV_LOG("Point %s is in cam frustum", testPos.string().c_str());
-
+        const Frustum camFrustum     = cam.GetFrustum();
         const Matrix4 viewProjection = cam.GetViewProjection();
 
         unlitShader.Use();
@@ -258,7 +375,6 @@ int main()
 
         if (camFrustum.Intersects(GetCubeBoundingBox(testModelMat)))
         {
-            SV_LOG("Cullable model is visible");
             unlitShader.SetUniformMat4("u_mvp", viewProjection * testModelMat);
             unlitShader.SetUniformVec4("u_tint", Color::yellow);
             DrawCube(vao);
@@ -269,23 +385,6 @@ int main()
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
-
-    //test events and inputs
-
-
-    //em.AddEvent<HelloWorldEvent>(hwEvent);
-    //em.Invoke<HelloWorldEvent>();
-
-    //ligEvent->Invoke(8, 9);
-    //em.Invoke<AddEvent>(9, 10);
-
-    //InputManager::KeyboardKeyType a(EKey::A, EKeyState::PRESSED, EInputModifier());
-    //InputManager::KeyCallback aF = [](int i) { std::cout << "Test A: " << i << std::endl; };
-
-    //im.AddInputBinding(a, aF);
-    //im.CallInput(a, 'a');
-
 
     return 0;
 }
